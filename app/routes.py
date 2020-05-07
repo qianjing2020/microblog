806062991dec711c1ed13261302b36a270b38c7b
qianjing2020 <qianjing2020@users.noreply.github.com>
2020-05-07 15:24:32 -0400

    updated database

--- app/routes.py
+++ app/routes.py
@@ -70,12 +70,12 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-# @app.route('/user/<username>')
-# @login_required
-# def user(username):
-#     user = User.query.filter_by(username=username).first_or_404()
-#     posts = [
-#         {'author': user, 'body': 'Test post #1'},
-#         {'author': user, 'body': 'Test post #2'}
-#     ]
-#     return render_template('user.html', user=user, posts=posts)
+@app.route('/user/<username>') # inside < > is a dynamic component
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
